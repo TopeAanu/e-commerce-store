3d42f808d3bd73cdbb48bd4e0503261be0efa6f5
TopeAanu <topefasasi99@gmail.com>
2025-07-31 21:25:25 +0100

    added favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,28 @@ export const metadata: Metadata = {
   title: "MelSore - Ecommerce Store",
   description:
     "Discover the best deals on electronics, fashion, home goods, and more at MelSore. Shop now for quality products at unbeatable prices.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -24,7 +46,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      {/* <body className={`${inter.className} overflow-x-hidden`}> */}
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/favicon-16x16.png"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          href="/favicon-32x32.png"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${inter.className} overflow-x-hidden`}
         style={{ maxWidth: "100vw", overflowX: "hidden" }}
